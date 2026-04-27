ae307e66ed17978db173bb1b4bebafb8e6fc38c7
megangrow <161376674+megangrow@users.noreply.github.com>
2026-04-27 18:16:30 -0500

    Add comments and error handling

--- player_s.py
+++ player_s.py
@@ -2,7 +2,7 @@
 import socket
 import json
 
-# set up server connection
+# set up server connection using TCP
 port = 5010
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -11,6 +11,7 @@ server.listen()
 c, address = server.accept()
 print(f"Connected with {address}")
 
+# dictionary of art for each hangman state
 hangman_pics = [
     # 0 wrong
     """
@@ -76,6 +77,8 @@ hangman_pics = [
       |
 ========="""
 ]
+
+# dictionary of the current state of the game
 current_state = {
     "word": "",
     "display_word": "",
@@ -87,17 +90,21 @@ current_state = {
     "hide_turn": False,
 }
 
-# initial setup
-def get_word():
+# initial setup helper functions
+def get_word(): # get word from server
     word = input("Enter a word: ")
     return list(word.upper())
 
-def create_display_word(word):
+def create_display_word(word): # fill in blanks based on word
     display_word = []
     for i in word:
-        display_word.append("_")
+        if i == " ":
+            display_word.append(" ") # display spaces if space
+        else:
+            display_word.append("_") # display blank if letter
     return display_word
 
+# create initial game state - client gets 5+number of letters turns to guess
 def create_initial():
     current_state["word"] = get_word()
     current_state["display_word"] = create_display_word(current_state["word"])
@@ -112,45 +119,48 @@ def send(client, state):
 def recv(client):
     return client.recv(1).decode()
 
-# make updates
-def change_picture():
-    current_state["mistakes"] += 1
+# if the client guessed the wrong letter:
+def wrong_guess_update():
+    current_state["mistakes"] += 1 # update mistake count
+    current_state["image"] = hangman_pics[current_state["mistakes"]]  # change hangman image
+    # if they've made two (more) mistakes, let the server choose a letter to rehide
     if current_state["mistakes"] % 2 == 0 and current_state["mistakes"] != 0:
         current_state["hide_turn"] = True
 
-    current_state["image"] = hangman_pics[current_state["mistakes"]]
-
+# update every round
 def update_state(l):
     updated = False
-    current_state["turns_left"] -= 1
+    current_state["turns_left"] -= 1 # use a turn
     for i in range(len(current_state["word"])):
-        if (l == current_state["word"][i]):
+        if (l == current_state["word"][i]): # if the letter is in the word, display
             current_state["display_word"][i] = l
             updated = True
-
+    # if it isn't, update the wrong guess
     if not updated:
-        change_picture()
+        wrong_guess_update()
     return
 
 # option for server to rehide certain letters after every 2 mistakes
 def rehide_letter():
+    # make sure there's letters uncovered to rehide
     if all(c == "_" for c in current_state["display_word"]):
         current_state["hide_turn"] = False
         return
 
     choice = input("Choose a letter to rehide: ").upper()
+    # loop through and re-hide the correct letter
     for i in range(len(current_state["word"])):
         if (choice == current_state["word"][i]):
             current_state["display_word"][i] = "_"
-
     current_state["hide_turn"] = False
 
+# check to see if a win (word uncovered) /loss (6 mistakes or all turns used) condition has been met
 def check_state():
     if current_state["mistakes"] > 5 or current_state["turns_left"] == 0:
         current_state["complete"] = True
         current_state["msg"] = "You lose!!!!"
 
-    if current_state["hide_turn"]:
+    if current_state["hide_turn"]: # let server rehide letter if its that turn
         rehide_letter()
 
     if ("_" not in current_state["display_word"]):
@@ -166,16 +176,19 @@ def pretty_print():
     print("\n---------------------------------------\n")
 
 if __name__ == "__main__":
-    create_initial()
+    try:
+        create_initial()
 
-    while not current_state["complete"]:
+        while not current_state["complete"]: # while the game is going
+            send(c, current_state)
+            letter = recv(c)
+
+            update_state(letter)
+            check_state()
+            pretty_print()
+
         send(c, current_state)
-        letter = recv(c)
 
-        update_state(letter)
-        check_state()
-        pretty_print()
-
-    send(c, current_state)
-
-
+    except (KeyboardInterrupt, ConnectionResetError, BrokenPipeError):
+        print("Disconnected from client")
+        exit()
